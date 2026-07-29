f0a808f29d66ca33f8b6ada6abd4b61ba622c4b2
Elif <elifgursoy02@gmail.com>
2026-07-29 14:11:43 +0300

    Fix indentation and formatting issues in calculator

--- velocityandsubstitutioncalc.py
+++ velocityandsubstitutioncalc.py
@@ -16,6 +16,7 @@ AverageVelocity=(LastVelocity+FirstVelocity)/2
 d=AverageVelocity*deltatime
 
 print(f"The substitution in the {deltatime}th second is {s:.2f}m which is maximum displacement in the whole movement of the object.")
+time.sleep(1)
 print(f"The maximum velocity of the object whole time is {FirstVelocity:} m/s.")
 
 
